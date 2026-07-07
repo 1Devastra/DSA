c9eaa46b8176d74ca60e71191c5a6af7f4aa5064
Dheeraj Singh <145746734+1Devastra@users.noreply.github.com>
2026-07-07 10:39:13 +0530

    Time: 1 ms (13.1%), Space: 9.2 MB (27.38%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     long long sumAndMultiply(int n) {
-        // std::vector<int> ans;
-        // for(int i=0; i<n; i++){
-        //     if(i!=0){
-        //         ans.push_back(i);
+        std::vector<int> ans;
+        while(n>0){
+           int digit=n%10;
+           if(digit!=0){
+            ans.push_back(digit);
+           }
+           n=n/10;
+        }
+        long long  x=0;
+        long long sum=0;
+        while(!ans.empty()){
+            int val =ans.back();
+            x=x*10+val;
+            sum+=val;
+            ans.pop_back();
+        }
+        return (long long)x*sum; 
+        // int x=0;
+        //  long long sum=0;
+        //  string s=to_string(n);
+        //  for(char c:s){
+        //     if(c!='0'){
+        //         int digit=c-'0';
+        //         x=x*10+digit;
+        //         sum=sum+digit;
         //     }
-        // }
-        // int  x=0;
-        // long long sum=0;
-        // while(!ans.empty()){
-        //     x=ans.back();
-        //     sum+=x;
-        //     ans.pop_back();
-        // }
-        // return (long long)x*sum; 
-        int x=0;
-         long long sum=0;
-         string s=to_string(n);
-         for(char c:s){
-            if(c!='0'){
-                int digit=c-'0';
-                x=x*10+digit;
-                sum=sum+digit;
-            }
-         }
-         return x*sum;
+        //  }
+        //  return x*sum;
 
     }
 };
